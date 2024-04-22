5877c52188d775282a82c9ccba1f151e04f4afce
Ole Tjernstad <ott556@gmail.com>
2024-04-22 13:13:44 +0200

    section me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="main-layout">
       {/* me */}
       <section className="section-me">
         <div className="image">
@@ -16,7 +16,28 @@ export default function Home() {
             alt="Min datter og meg"
           />
         </div>
-        <div className="me"></div>
+        <div className="me">
+          <p className="greet">Hei !!</p>
+          <p>
+            Jeg heter Ole Tjernstad, innehaver av Tjernstad Utvikling og
+            selvlært hobby utvikler. I tillegg til egen lesing har jeg fullført
+            og bestått et javascript kurs ved{" "}
+            <a
+              href="https://teamtreehouse.com/techdegree/full-stack-javascript"
+              rel="nofollow"
+              target="_blank"
+            >
+              treehouse
+            </a>
+            .
+          </p>
+          <p>
+            Videre her vil jeg vise frem noen av prosjektene jeg har jobbet med.
+          </p>
+          <p>
+            Og viktigst, jeg er også pappa til ei nydelig jente, bare se selv!!
+          </p>
+        </div>
       </section>
     </main>
   );
