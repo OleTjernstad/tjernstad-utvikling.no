cb0676e29ffbb5c89ff0e44b9a959b116df2b6cc
Ole Tjernstad <ott556@gmail.com>
2024-04-28 22:19:46 +0200

    empty alt

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -5,5 +5,5 @@ interface IconProps {
 }
 export function Icon({ name }: IconProps) {
   const nme = name.toLowerCase();
-  return <Image src={`/icons/${nme}.svg`} alt={name} width={20} height={20} />;
+  return <Image src={`/icons/${nme}.svg`} alt="" width={20} height={20} />;
 }
